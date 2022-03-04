383fd631aa246d001402cac42c532cbd8596b94a
EltonA95 <100603162+EltonA95@users.noreply.github.com>
2022-03-04 16:16:03 +0100

    Update Credenziali.py

--- Funzioni/Credenziali.py
+++ Funzioni/Credenziali.py
@@ -1,14 +1,11 @@
-"""in questo flie scriverò tutte le credenziali che mi servono, che siano
-di flickr, o google."""
+"""Insert yoyr keys in this file"""
 
 
-googleKey = "AIzaSyChaoKVp6R1DVRx4atIDV55GxyvoPlD448"
-    #"AIzaSyBoUy9Q7BB6Tl70ZlA0_TlSr8yQxLtSWRM" vecchia chiave
-flickrKey = "708e1bf66437737bf86c19a4970ee1d9"
-flickrSecret = '6528a8585689bc06'
+googleKey = ""
+   
+flickrKey = ""
+flickrSecret = ''
 
-roadApi = "AIzaSyChaoKVp6R1DVRx4atIDV55GxyvoPlD448"
-    #"AIzaSyCG1kE38yy_ewYmlqYJjZ3qx6m2HrFvTGs" #vecchia
 
 """il scade la prova gratuita delle api google, quindi è opportuno cambiare account ogni 3 mesi
 in modo tale da avere l'API gratuita"""
